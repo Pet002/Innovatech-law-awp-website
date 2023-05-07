60cae6ef3859cfaf9c2430bd1009285981b02a6a
Pet002 <pet.14336@gmail.com>
2023-05-07 19:25:33 +0700

    add fivcon

--- components/FooterBar/index.js
+++ components/FooterBar/index.js
@@ -5,74 +5,91 @@ import Logo from "../../assets/logo.png";
 import styles from "../../styles/Footer.module.css";
 
 const FooterBar = () => {
-    const router = useRouter();
-    var page = router.pathname;
-    return (
-        <footer className={`${styles.Footertab}`}>
-            <div className={`${'container'}`} >
-                <div className="row">
-                    <div className="col-md-4 mx-md-auto my-5" >
-                        <Image src={Logo} alt="logo" width={150} height={150} />
-                        <p className={`${styles.text}`}>
-                        41-43 soi.ladprao 55 saphan-song <br/>
-                        subdistrict wang thonglang district   bangkok, Thailand
-                        </p>
-                    </div>
-                    <div className="col-md-4 my-md-5">
-                        <div className="row">
-                            <h4 className={`${styles.text} `}>
-                                เมนู
-                            </h4>
-                            <Link href="/">
-                                <a className={`${styles.footLink} ${"my-1"} ${(page == "/") ? (styles.linkActive) : ("")}`}>
-                                    หน้าหลัก
-                                </a>
-                            </Link>
-                            <Link href="/ourService">
-                                <a className={`${styles.footLink} ${"my-1"} ${(page == "/ourService") ? (styles.linkActive) : ("")}`}>
-                                    Our Service
-                                </a>
-                            </Link>
-                            <Link href="/aboutUs">
-                                <a className={`${styles.footLink} ${"my-1"} ${(page == "/aboutUs") ? (styles.linkActive) : ("")}`}>
-                                    About Us
-                                </a>
-                            </Link>
-                            <Link href="/executiveProfile">
-                                <a className={`${styles.footLink} ${"my-1"} ${(page == "/executiveProfile") ? (styles.linkActive) : ("")}`}>
-                                    Executive Profile
-                                </a>
-                            </Link>
-                        </div>
-                    </div>
-                    <div className="col-md-4 my-5">
-                        <div className="row">
-                            <h5 className={`${styles.text} `}>
-                                ติดต่อเรา
-                            </h5>
-
-                            <a className={`${styles.footLink} ${"my-1"} ${styles.linkActive}`} href="mailto: Innovatechlaw.awp@gmail.com">
-                                Innovatechlaw.awp@gmail.com
-                            </a>
-                            <span itemProp="telephone">
-                                <a className={`${styles.footLink} ${"my-1"} ${styles.linkActive}`} href="tel: 083-516-2915">
-                                    083-516-2915
-                                </a>
-                            </span>
-
-                        </div>
-                    </div>
-                </div>
-                <hr/>
-                <div className="row">
-                    <div className={`${"col-md-12 text-center"} ${styles.rightCopy}`}>
-                        <p>
-                        © {new Date().getFullYear()}  All Rights Reserved
-                        </p>
-                    </div>
-                </div>
+  const router = useRouter();
+  var page = router.pathname;
+  return (
+    <footer className={`${styles.Footertab}`}>
+      <div className={`${"container"}`}>
+        <div className="row">
+          <div className="col-md-4 mx-md-auto my-5">
+            <Image src={Logo} alt="logo" width={150} height={150} />
+            <p className={`${styles.text}`}>
+              41-43 soi.ladprao 55 saphan-song <br />
+              subdistrict wang thonglang district bangkok, Thailand
+            </p>
+          </div>
+          <div className="col-md-4 my-md-5">
+            <div className="row">
+              <h4 className={`${styles.text} `}>เมนู</h4>
+              <Link href="/">
+                <a
+                  className={`${styles.footLink} ${"my-1"} ${
+                    page == "/" ? styles.linkActive : ""
+                  }`}
+                >
+                  หน้าหลัก
+                </a>
+              </Link>
+              <Link href="/ourService">
+                <a
+                  className={`${styles.footLink} ${"my-1"} ${
+                    page == "/ourService" ? styles.linkActive : ""
+                  }`}
+                >
+                  Our Service
+                </a>
+              </Link>
+              <Link href="/aboutUs">
+                <a
+                  className={`${styles.footLink} ${"my-1"} ${
+                    page == "/aboutUs" ? styles.linkActive : ""
+                  }`}
+                >
+                  About Us
+                </a>
+              </Link>
+              <Link href="/executiveProfile">
+                <a
+                  className={`${styles.footLink} ${"my-1"} ${
+                    page == "/executiveProfile" ? styles.linkActive : ""
+                  }`}
+                >
+                  Executive Profile
+                </a>
+              </Link>
             </div>
-        </footer>
-    )
-}
+          </div>
+          <div className="col-md-4 my-5">
+            <div className="row">
+              <h5 className={`${styles.text} `}>ติดต่อเรา</h5>
+
+              <a
+                className={`${styles.footLink} ${"my-1"} ${styles.linkActive}`}
+                href="mailto: Info@innovatechlawawp.com"
+              >
+                Info@innovatechlawawp.com
+              </a>
+              <span itemProp="telephone">
+                <a
+                  className={`${styles.footLink} ${"my-1"} ${
+                    styles.linkActive
+                  }`}
+                  href="tel: 083-516-2915"
+                >
+                  083-516-2915
+                </a>
+              </span>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          <div className={`${"col-md-12 text-center"} ${styles.rightCopy}`}>
+            <p>© {new Date().getFullYear()} All Rights Reserved</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 export default FooterBar;
