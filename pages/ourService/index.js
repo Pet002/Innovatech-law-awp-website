15bc3a0b3c71a46abd6a2cb69b921d637ebfcfa7
Pet002 <pet.14336@gmail.com>
2023-06-23 13:36:22 +0700

    :truck: Update ourservice
    i update ourservice to a new format

--- pages/ourService/index.js
+++ pages/ourService/index.js
@@ -1,29 +1,32 @@
-import Image from 'next/image'
-import styles from '../../styles/ourService.module.css'
-//Image SVG 
-import startSME from '../../assets/SVG/startSME.svg'
-import Note from '../../assets/SVG/Note.svg'
-import script from '../../assets/SVG/script.svg'
-import success from '../../assets/SVG/success.svg'
-import TAX from '../../assets/SVG/TAX.svg'
-import VAT from '../../assets/SVG/VAT.svg'
-import Hummer from '../../assets/SVG/Hummer.svg'
-import Card from '../../components/Card'
+import Image from "next/image";
+import styles from "../../styles/ourService.module.css";
+//Image SVG
+import startSME from "../../assets/SVG/startSME.svg";
+import Note from "../../assets/SVG/Note.svg";
+import script from "../../assets/SVG/script.svg";
+import success from "../../assets/SVG/success.svg";
+import TAX from "../../assets/SVG/TAX.svg";
+import VAT from "../../assets/SVG/VAT.svg";
+import Hummer from "../../assets/SVG/Hummer.svg";
+import Card from "../../components/Card";
 
+import hummerOutline from "../../assets/Service/lawer-firstpage.svg"
+import court from "../../assets/Service/court.svg"
 
-import Meta from '../../components/meta'
+import Meta from "../../components/meta";
 
 export default function OutServices() {
+  const test = () => {
+    console.log("test");
+  };
   return (
-    <div className='container'>
-      <Meta pageTitle="บริการของเรา"/>
+    <div className="container">
+      <Meta pageTitle="บริการของเรา" />
       <main className={styles.main}>
-        <div className='row'>
-          <h2 className={`${'text-center'} ${'mb-3'}`}>
-            บริการของเรา
-          </h2>
+        <div className="row">
+          <h2 className={`${"text-center"} ${"mb-3"}`}>บริการของเรา</h2>
         </div>
-        <div className='row'>
+        {/* <div className='row'>
           <Card images={startSME} altImages="SME" title="บริการธุรกิจSME และธุรกิจสตาทอัพ" text="ในโลกยุคใหม่ การค้าขาย การทำธุรกิจ เป็นออนไลน์กันหมดแทบจะ 100% ทำให้มีช่องทางการถูกเอารัดเอาเปรียบจากคนบางกลุ่มได้ง่าย และกฎหมายใดบ้างล่ะที่เกี่ยวข้องกับธุรกิจสตาทอัพของคุณเพื่อที่จะคุ้มครองสิทธิของ คุณก็ขึ้นอยู่กับว่าคุณทำธุรกิจเกี่ยวข้องกับอะไรส่งข้อความหาเรา เพื่อขอคำปรึกษากับเราโดยไม่เสียค่าใช้จ่าย และเราจะติดต่อกลับให้เร็วที่สุด " link="/ourService/SME" />
           <Card images={success} altImages="correct" title="บริการด้านกฎหมายทรัพย์ทางปัญญา" text="ทรัพย์สินทางปัญญา (Intellectual Property) ผลงานอันเกิดจากการ ประดิษฐ์ คิดค้น หรือสร้างสรรค์ของมนุษย์ ซึ่งเน้นที่ผลผลิตของสติปัญญาและความชำนาญ โดยไม่คำนึงถึงชนิดของการสร้างสรรค์หรือวิธีใน การแสดงออก เช่น สินค้าต่าง ๆ การบริการ กรรมวิธีการผลิตทางอุตสาหกรรม เป็นต้น" link="/ourService" />
           <Card images={script} altImages="paper" title="บริการจดทะเบียนธุรกิจทุกประเภท" text="&nbsp&nbsp&nbsp&nbsp&nbsp มีได้แก่ <br/>
@@ -40,9 +43,39 @@ export default function OutServices() {
           <Card images={TAX} altImages="TAX" title="บริการจดทะเบียนภาษีธุรกิจเฉพาะ" text="ภาษีที่เก็บจากกิจการบางประเภทที่กฎหมายกำหนดเป็นพิเศษแยกจากภาษีมูลค่าเพิ่ม โดยกรมสรรพากรเป็นผู้จัดเก็บจากธุรกิจที่เป็นบุคคลธรรมดาหรือนิติบุคคล โดยผู้ประกอบการที่มีหน้าที่เสียภาษีธุรกิจเฉพาะ ต้องยื่นคำขอจดทะเบียนภาษีธุรกิจเฉพาะด้วยแบบคำขอ ภ.ธ.01 ภายใน 30 วันนับแต่วันเริ่มกิจการ" link="/" />
           <Card images={Hummer} altImages="Judge Hammer" title="บริการดำเนินคดีทางศาล" text="ในการดำเนินคดีทางกฎหมายทางเราได้มีทนายที่จะช่วยเหลือคุณในการจัดการทางด้านกฎหมายมากความสามารถ โดยเรามีความสามารถทั้งในด้าน คดีแพ่ง คดีอาญา คดีแรงงาน คดีมรดก คดีครอบครัว คดีคุ้มครองผู้บริโภค " link="/" />
           
+        </div> */}
+        <div className={`${styles.contentBlock}`}>
+          <div className={`${styles.card}`} onClick={test} tabIndex={1}>
+            <div className={`${styles.cardImage}`}>
+              <Image src={hummerOutline} height={100} width={200} />
+            </div>
+            <h4 className={`${styles.cardTitle}`}>บริการทางกฎหมาย</h4>
+          </div>
+
+          <div className={`${styles.card}`} onClick={test} tabIndex={2}>
+            <div className={`${styles.cardImage}`}>
+              <Image src={court} height={100} width={200} />
+            </div>
+            <h4 className={`${styles.cardTitle}`}>บริการดำเนินคดี/ทางศาล</h4>
+          </div>
+
+          <div className={`${styles.card}`} onClick={test} tabIndex={3}>
+            <div className={`${styles.cardImage}`}>
+              <Image src={Note} height={100} width={200} />
+            </div>
+            <h4 className={`${styles.cardTitle}`}>บริการเกี่ยวกับบัญชี</h4>
+          </div>
+
+          {/* <div className={`${styles.card}`} onClick={test} tabIndex={4}>
+            <div className={`${styles.cardImage}`}>
+              <Image src={Note} height={100} width={200} />
+            </div>
+            <h4 className={`${styles.cardTitle}`}>บริการทางกฎหมาย</h4>
+          </div> */}
+          
+          
         </div>
       </main>
-
     </div>
-  )
+  );
 }
